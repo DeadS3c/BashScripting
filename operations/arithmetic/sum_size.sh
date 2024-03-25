3a951177f8be45f6781aa93cf61212cc073d5426
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2024-03-25 11:01:01 +0100

    Create sum_size.sh

---
+++ operations/arithmetic/sum_size.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Calcula la suma del tamaño de 2 ficheros
+
+fs1=$(du -b $1 | cut -f1)
+fs2=$(du -b $2 | cut -f1)
+
+echo "File size of $1 is: $fs1"
+echo "File size of $2 is: $fs2"
+
+total=$(($fs1 + $fs2))
+
+echo "Total size is: $total"
