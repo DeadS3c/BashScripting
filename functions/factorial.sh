d4bf802ca8d95d883671ebafd9a1b136ac4eedab
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2024-03-27 09:35:52 +0100

    Create factorial.sh

---
+++ functions/factorial.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+factorial () {
+	if [ $1 -le 1 ]; then
+		echo 1
+	else
+		last=$(factorial $(( $1 -1)))
+		echo $(( $1 * last ))
+	fi
+}
+
+echo -n "4! is: "
+factorial 4
+echo -n "5! is: "
+factorial 5
